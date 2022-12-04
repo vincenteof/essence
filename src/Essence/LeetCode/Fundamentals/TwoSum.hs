3f7a77ecb7516ec0553023829cc2fe9bd3b8f2c8
vincenteof <14847208+vincenteof@users.noreply.github.com>
2022-12-04 23:46:45 +0800

    two sum

--- src/Essence/LeetCode/Fundamentals/TwoSum.hs
+++ src/Essence/LeetCode/Fundamentals/TwoSum.hs
@@ -1,4 +1,22 @@
-module Essence.LeetCode.Fundamentals.TwoSum where
+module Essence.LeetCode.Fundamentals.TwoSum
+  ( twoSum
+  ) where
 
-twoSum :: [Int] -> Int -> [Int]
-twoSum arr _ = arr
+import           Data.Map                       ( Map
+                                                , empty
+                                                , findWithDefault
+                                                , insert
+                                                )
+
+twoSum :: [Int] -> Int -> (Int, Int)
+twoSum xs = twoSumIter (zip xs [1 ..]) empty
+
+
+twoSumIter :: [(Int, Int)] -> Map Int Int -> Int -> (Int, Int)
+twoSumIter xs records target
+  | anotherIndex >= 0 = (anotherIndex, curIdx)
+  | otherwise         = twoSumIter (tail xs) (insert cur curIdx records) target
+ where
+  (cur, curIdx) = head xs
+  another       = target - cur
+  anotherIndex  = findWithDefault (-1) another records
